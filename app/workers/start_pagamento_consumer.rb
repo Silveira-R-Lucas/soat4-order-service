57885470b08ca56bf62c8aafbc05baa2bbaddab3
Lucas Silveira <silveira.codes@gmail.com>
2025-12-19 00:47:58 -0300

    solving rabbit consumer bug on ci/cd

--- app/workers/start_pagamento_consumer.rb
+++ app/workers/start_pagamento_consumer.rb
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 
-RabbitmqConsumer.new('pagamento.events', 'order-service.pagamento').start_listening
+class StartPagamentoConsumer
+  def self.run
+    RabbitmqConsumer.new('pagamento.events', 'order-service.pagamento').start_listening
+  end
+end
